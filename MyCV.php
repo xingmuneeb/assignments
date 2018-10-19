eb37aff7f1dd3776e71cf02fed674e4467dbcfea
xingmuneeb <42598174+xingmuneeb@users.noreply.github.com>
2018-10-18 23:59:14 -0600

    Add files via upload
    CV Index File

---
+++ MyCV.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Resume</title>
+	<link rel="stylesheet" type="text/css" href="styling/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+
+
+<div class='resume'>
+	
+<p class='resume_head'>Resume</p>
+
+</div>
+<div class='container'>
+<div class='mydetails'>
+
+	<div class='prodetails'>
+	Professional Details
+	<div class='aboutme'><p class=abtme>About Me</p>
+
+	</div>
+
+</div>
+<hr width='70%'>
+</div>
+	<div class='pdetails'>
+	    <p id='littledetails'>Name</p>             
+		<p id='littledetails'>Degree </p>       
+		<p id='littledetails'>Phone</p>      
+		<p id='littledetails'>Email</p>
+		
+		</div>
+		<div id='dans'>
+			<p id='ans'>Muneeb</p>
+			<p id='ans'>BSIT</p>
+			<p id='ans'>03xx-xxxxxxx</p>
+			<p id='ans'>xinggmuneeb@gmail.com</p>
+		</div>  
+		<div class='pdetails2'>
+	    <p id='littledetails'>Age</p>             
+		<p id='littledetails'>Experience </p>       
+		<p id='littledetails'>Career Level</p>      
+		<p id='littledetails'>Website</p>
+		</div>
+		<div id='dans2'>
+			<p id='ans2'>21</p>
+			<p id='ans2'>Fresher</p>
+			<p id='ans2'>Mid-Level</p>
+		    <p id='ans2'><a href='https://www.hackmydevice.com'>HackMyDevice.com</a></p>
+		    <div class='propic'><img src='DSC_0085.JPG'></div>
+			
+		</div>
+		<div class='group'>
+		<div id='AboutMee'>About Me</div>
+		<hr >
+		<div class='paracontainer'><p id='para'>My Name is Muneeb From Pakistan. My Town address is Qila Ahmad Abad District Narowal. I am stufying BSIT in Murray College Sialkot. My Roll No. is 039. I want to be a professional web developer. I am passionate and hardworking.
+		My Skills are:
+		<ul class='skills'>	
+		<li>PhotoShop</li>
+		<li>HTML5/CSS3</li>
+		<li>Hacking</li>
+			<li>Penetration Testing</li>
+	</ul>
+	</p> </div></div>
+	
+		<p id='detailupic'>Name</p>
+		<p id='detailupic'>Degree </p>       
+		<p id='detailupic'>Phone</p>      
+		<p id='detailupic'>Email</p>
+		<p id='detailupic'>Age</p>             
+		<p id='detailupic'>Experience </p>       
+		<p id='detailupic'>Career Level</p>      
+		<p id='detailupic'>Website</p>
+		</div>
+		<div id='pdetailsupic1'>
+			<p id='pdetailsupic'>Muneeb</p>
+			<p id='pdetailsupic'>BSIT</p>
+			<p id='pdetailsupic'>03xx-xxxxxxx</p>
+			<p id='pdetailsupic'>xinggmuneeb@gmail.com</p>
+			<p id='pdetailsupic'>21</p>
+			<p id='pdetailsupic'>Fresher</p>
+			<p id='pdetailsupic'>Mid-Level</p>
+		    <p id='pdetailsupic'><a href='https://www.hackmydevice.com'>HackMyDevice.com</a></p>
+
+		</div>  
+		
+</body>
+</html>
